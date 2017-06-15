6709ea66202ee1fce18b00ca179281bc518049ab
TacoHsien <tacothriller8352@gmail.com>
2017-06-15 14:59:00 +0800

    Add Bin Picking related libraries

---
+++ techman_robot/tm700_test/src/tm700_realsense.cpp
@@ -0,0 +1,256 @@
+/*********************************************************************
+ * tm700_test_node.cpp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *********************************************************************
+ *
+ * Author: Howard Chen, ISCI, NCTU
+ */
+
+#include <ros/ros.h>
+#include <ros/console.h>
+
+#include <moveit/move_group_interface/move_group.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit/robot_state/robot_state.h>
+#include <moveit/robot_state/conversions.h>
+
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/CollisionObject.h>
+
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+
+#include "tm_msgs/SetIO.h"
+//#include "tm_msgs/SetIORequest.h"
+//#include "tm_msgs/SetIOResponse.h"
+
+//ROS PCL Library
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+
+//Bin Picking related Libraries
+#include "CADDatabaseClass.h"
+#include "PCD_Function.h"
+#include "PCDProcessClass.h"
+#include "VotingSchemePoseEstimation_Class.h"
+
+ros::Publisher pub;
+//Set data path
+std::string pcd_data_path = "/home/isci/Documents/tm5_ws/src/isci_ros_tm5/techman_robot/tm700_test/pcd/";
+std::string pcd_save_path = "/home/isci/Documents/tm5_ws/src/isci_ros_tm5/techman_robot/tm700_test/src/data/";
+
+void cloud_cb(const sensor_msgs::PointCloud2Ptr& input)
+{
+  //Create a container for the data.
+  sensor_msgs::PointCloud2 output;
+
+  //Do data processing here
+  output = *input;
+
+  //Publish the data.
+  ROS_INFO("Publish output");
+  pub.publish(output);
+}
+
+bool try_move_to_named_target(moveit::planning_interface::MoveGroup& group,
+                              moveit::planning_interface::MoveGroup::Plan& plan,
+                              const std::string& target_name,
+                              unsigned int max_try_times = 1
+                             )
+{
+    if (!ros::ok()) return false;
+    bool success = false;
+
+    for (unsigned int i = 0; i < max_try_times; i++)
+    {
+
+        group.setNamedTarget(target_name);
+
+        if (group.move())
+        {
+            success = true;
+            break;
+        }
+        else
+        {
+            if (!ros::ok()) break;
+            sleep(1);
+        }
+    }
+    return success;
+}
+
+bool try_move_to_joint_target(moveit::planning_interface::MoveGroup& group,
+                              moveit::planning_interface::MoveGroup::Plan& plan,
+                              const std::vector<double>& joint_target,
+                              unsigned int max_try_times = 1
+                             )
+{
+    if (!ros::ok()) return false;
+    bool success = false;
+
+    for (unsigned int i = 0; i < max_try_times; i++)
+    {
+
+        group.setJointValueTarget(joint_target);
+
+        if (group.move())
+        {
+            success = true;
+            break;
+        }
+        else
+        {
+            if (!ros::ok()) break;
+            sleep(1);
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+
+    ros::init(argc, argv, "tm700_realsense");
+    ros::NodeHandle node_handle, node_handle_file;
+    sensor_msgs::PointCloud2 pcd_data;
+    pcl::PointCloud<pcl::PointXYZ> cloud;
+
+    //Create a ROS subscriber for the input point cloud
+    ROS_INFO("Subscribe /camera/depth/points");
+    ros::Subscriber sub = node_handle.subscribe("/camera/depth/points", 10, cloud_cb);
+
+    //Create ROS publishe
+    ros::Publisher pcl_pub = node_handle_file.advertise<sensor_msgs::PointCloud2>("pcl_output", 10);
+
+    //Load PCD file
+    pcl::io::loadPCDFile(pcd_data_path + "CADObject_0_theta_0_phi_0.pcd", cloud);
+    pcl::io::savePCDFileASCII("test.pcd", cloud);
+    pcl::toROSMsg(cloud, pcd_data);
+    pcd_data.header.frame_id = "point_cloud";
+
+    //Create a ROS publisher for the output point cloud
+    pub = node_handle.advertise<sensor_msgs::PointCloud2>("output", 10);
+/*
+    ros::ServiceClient set_io_client = node_handle.serviceClient<tm_msgs::SetIO>("tm_driver/set_io");
+    tm_msgs::SetIO io_srv;
+    io_srv.request.fun = 2;//tm_msgs::SetIORequest::FUN_SET_EE_DIGITAL_OUT;
+    io_srv.request.ch = 0;
+    io_srv.request.value = 0.0;
+*/
+    // start a background "spinner", so our node can process ROS messages
+    //  - this lets us know when the move is completed
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
+    while(ros::ok())
+    {
+        //ROS_INFO("Publish pcd_data");
+        //pcl_pub.publish(pcd_data);
+    }
+/*
+    sleep(1);
+
+    // Setup
+    // ^^^^^
+    // The :move_group_interface:`MoveGroup` class can be easily
+    // setup using just the name
+    // of the group you would like to control and plan for.
+    moveit::planning_interface::MoveGroup group("manipulator");
+    // We will use the :planning_scene_interface:`PlanningSceneInterface`
+    // class to deal directly with the world.
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+    // (Optional) Create a publisher for visualizing plans in Rviz.
+    //ros::Publisher display_publisher = node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
+    //moveit_msgs::DisplayTrajectory display_trajectory;
+
+    // Getting Basic Information
+    // ^^^^^^^^^^^^^^^^^^^^^^^^^
+    // We can print the name of the reference frame for this robot.
+    ROS_INFO("Reference frame: %s", group.getPlanningFrame().c_str());
+    // We can also print the name of the end-effector link for this group.
+    ROS_INFO("Reference frame: %s", group.getEndEffectorLink().c_str());
+
+    moveit::planning_interface::MoveGroup::Plan my_plan;
+
+    set_io_client.call(io_srv);
+
+    group.setPlanningTime(30.0);
+
+    try_move_to_named_target(group, my_plan, "home", 100);
+
+    std::vector<double> joint_target_1;
+    std::vector<double> joint_target_2;
+
+    joint_target_1.assign(6, 0.0f);
+    joint_target_1[0] = 0.0174533 * ( 30.0);
+    joint_target_1[1] = 0.0174533 * ( 15.0);
+    joint_target_1[2] = 0.0174533 * (105.0);
+    joint_target_1[3] = 0.0174533 * (-30.0);
+    joint_target_1[4] = 0.0174533 * ( 90.0);
+    joint_target_1[5] = 0.0174533 * ( 30.0);
+
+    joint_target_2.assign(6, 0.0f);
+    joint_target_2[0] = 0.0174533 * (-30.0);
+    joint_target_2[1] = 0.0174533 * (-15.0);
+    joint_target_2[2] = 0.0174533 * ( 90.0);
+    joint_target_2[3] = 0.0174533 * (-75.0);
+    joint_target_2[4] = 0.0174533 * (120.0);
+
+    int step = 0;
+
+    while (ros::ok())
+    {
+        switch (step)
+        {
+        case 0: //move to ready1
+            ROS_INFO("move...to ready1");
+            try_move_to_named_target(group, my_plan, "ready1", 100);
+            break;
+        case 1: //move to ready2
+            ROS_INFO("move...to ready2");
+            try_move_to_named_target(group, my_plan, "ready2", 100);
+            break;
+        case 2: //move to ready3
+            ROS_INFO("move...to ready3");
+            try_move_to_named_target(group, my_plan, "ready3", 100);
+            break;
+        case 3: //move to ready4
+            ROS_INFO("move...to ready4");
+            try_move_to_named_target(group, my_plan, "ready4", 100);
+            break;
+
+        case 4: //move 1
+            ROS_INFO("move...to joint_target_1");
+            try_move_to_joint_target(group, my_plan, joint_target_1, 100);
+            break;
+
+        case 5: //move 2...
+            ROS_INFO("move...to joint_target_2");
+            try_move_to_joint_target(group, my_plan, joint_target_2, 100);
+            break;
+        }
+        step = (step + 1) % 6;
+    }
+*/
+    return 0;
+}
